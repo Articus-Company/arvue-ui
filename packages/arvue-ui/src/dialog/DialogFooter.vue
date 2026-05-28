502eed27164a3c462e1458d727a5776a312ff2d3
Xalindie <ilyaserdyukovmmm@gmail.com>
2026-05-28 18:02:09 +1100

    fix: add primitive props to dialog footer and header

--- packages/arvue-ui/src/dialog/DialogFooter.vue
+++ packages/arvue-ui/src/dialog/DialogFooter.vue
@@ -3,22 +3,29 @@
 </style>
 
 <template>
-    <Primitive :class="clsx('arvue-dialog-footer', props.class)">
+    <Primitive
+        :class="clsx('arvue-dialog-footer', props.class)"
+        v-bind="delegatedProps"
+    >
         <slot/>
     </Primitive>
 </template>
 
 <script lang="ts">
+import type { PrimitiveProps } from 'reka-ui'
 import type { HTMLAttributes } from 'vue'
 
-export interface DialogFooterProps {
+export interface DialogFooterProps extends PrimitiveProps {
     class?: HTMLAttributes['class']
 }
 </script>
 
 <script setup lang="ts">
+import { reactiveOmit } from '@vueuse/core'
 import { clsx } from 'clsx'
 import { Primitive } from 'reka-ui'
 
 const props = defineProps<DialogFooterProps>()
+
+const delegatedProps = reactiveOmit(props, 'class')
 </script>
--- packages/arvue-ui/src/dialog/DialogHeader.vue
+++ packages/arvue-ui/src/dialog/DialogHeader.vue
@@ -3,22 +3,29 @@
 </style>
 
 <template>
-    <Primitive :class="clsx('arvue-dialog-header', props.class)">
+    <Primitive
+        :class="clsx('arvue-dialog-header', props.class)"
+        v-bind="delegatedProps"
+    >
         <slot/>
     </Primitive>
 </template>
 
 <script lang="ts">
+import type { PrimitiveProps } from 'reka-ui'
 import type { HTMLAttributes } from 'vue'
 
-export interface DialogHeaderProps {
+export interface DialogHeaderProps extends PrimitiveProps {
     class?: HTMLAttributes['class']
 }
 </script>
 
 <script setup lang="ts">
+import { reactiveOmit } from '@vueuse/core'
 import { clsx } from 'clsx'
 import { Primitive } from 'reka-ui'
 
 const props = defineProps<DialogHeaderProps>()
+
+const delegatedProps = reactiveOmit(props, 'class')
 </script>
