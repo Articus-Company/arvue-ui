8c67dc2bc92e08559e5f40f37ec433676ee286b1
Xalindie <ilyaserdyukovmmm@gmail.com>
2026-05-29 06:23:40 +1100

    feat: add sheet component

---
+++ packages/arvue-ui/src/sheet/SheetFooter.vue
@@ -0,0 +1,31 @@
+<style>
+@import "sheet-footer.css";
+</style>
+
+<template>
+    <Primitive
+        :class="clsx('arvue-sheet-footer', props.class)"
+        v-bind="delegatedProps"
+    >
+        <slot/>
+    </Primitive>
+</template>
+
+<script lang="ts">
+import type { PrimitiveProps } from 'reka-ui'
+import type { HTMLAttributes } from 'vue'
+
+export interface SheetFooterProps extends PrimitiveProps {
+    class?: HTMLAttributes['class']
+}
+</script>
+
+<script setup lang="ts">
+import { reactiveOmit } from '@vueuse/core'
+import { clsx } from 'clsx'
+import { Primitive } from 'reka-ui'
+
+const props = defineProps<SheetFooterProps>()
+
+const delegatedProps = reactiveOmit(props, 'class')
+</script>
---
+++ packages/arvue-ui/src/sheet/SheetOverlay.vue
@@ -0,0 +1,31 @@
+<style>
+@import 'sheet-overlay.css';
+</style>
+
+<template>
+    <DrawerOverlay
+        :class="clsx('arvue-sheet-overlay', props.class)"
+        v-bind="delegatedProps"
+    >
+        <slot/>
+    </DrawerOverlay>
+</template>
+
+<script lang="ts">
+import type { DialogOverlayProps } from 'reka-ui'
+import type { HTMLAttributes } from 'vue'
+
+export interface SheetOverlayProps extends DialogOverlayProps {
+    class?: HTMLAttributes['class']
+}
+</script>
+
+<script setup lang="ts">
+import { reactiveOmit } from '@vueuse/core'
+import { clsx } from 'clsx'
+import { DrawerOverlay } from 'vaul-vue'
+
+const props = defineProps<SheetOverlayProps>()
+
+const delegatedProps = reactiveOmit(props, 'class')
+</script>
