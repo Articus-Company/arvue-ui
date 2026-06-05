843db368e771b3d38b5ab1a3ed96d5a1a1a264c7
Ilya Serdyukov <ilyaserdyukovmmm@gmail.com>
2026-06-06 04:25:30 +1100

    feat: add accordion component (#14)

---
+++ packages/arvue-ui/src/accordion/Accordion.vue
@@ -0,0 +1,30 @@
+<template>
+    <AccordionRoot
+        v-slot="slotProps"
+        v-bind="forwarded"
+    >
+        <slot v-bind="slotProps"/>
+    </AccordionRoot>
+</template>
+
+<script lang="ts">
+import type { AccordionRootEmits, AccordionRootProps } from 'reka-ui'
+
+export interface AccordionProps extends AccordionRootProps {
+}
+
+export interface AccordionEmits extends AccordionRootEmits {
+}
+</script>
+
+<script setup lang="ts">
+import {
+    AccordionRoot,
+    useForwardPropsEmits,
+} from 'reka-ui'
+
+const props = defineProps<AccordionProps>()
+const emits = defineEmits<AccordionEmits>()
+
+const forwarded = useForwardPropsEmits(props, emits)
+</script>
---
+++ packages/arvue-ui/src/accordion/AccordionContent.vue
@@ -0,0 +1,33 @@
+<style>
+@import './accordion-content.css';
+</style>
+
+<template>
+    <AccordionContent
+        v-bind="delegatedProps"
+        class="arvue-accordion-content"
+    >
+        <div :class="clsx('arvue-accordion-content-block', props.class)">
+            <slot/>
+        </div>
+    </AccordionContent>
+</template>
+
+<script lang="ts">
+import type { AccordionContentProps as RekaAccordionContentProps } from 'reka-ui'
+import type { HTMLAttributes } from 'vue'
+
+export interface AccordionContentProps extends RekaAccordionContentProps {
+    class?: HTMLAttributes['class']
+}
+</script>
+
+<script setup lang="ts">
+import { reactiveOmit } from '@vueuse/core'
+import { clsx } from 'clsx'
+import { AccordionContent } from 'reka-ui'
+
+const props = defineProps<AccordionContentProps>()
+
+const delegatedProps = reactiveOmit(props, 'class')
+</script>
