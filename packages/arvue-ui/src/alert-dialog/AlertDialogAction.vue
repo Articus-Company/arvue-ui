6269ba2b7ab7d4cc02ebf34b96617c83409e6bec
Ilya Serdyukov <ilyaserdyukovmmm@gmail.com>
2026-06-13 10:47:03 +1100

    feat: add alert dialog component (#18)

---
+++ packages/arvue-ui/src/alert-dialog/AlertDialogAction.vue
@@ -0,0 +1,19 @@
+<template>
+    <AlertDialogAction v-bind="forwarded">
+        <slot/>
+    </AlertDialogAction>
+</template>
+
+<script lang="ts">
+import type { AlertDialogActionProps as RekaAlertDialogActionProps } from 'reka-ui'
+
+export interface AlertDialogActionProps extends RekaAlertDialogActionProps {}
+</script>
+
+<script setup lang="ts">
+import { AlertDialogAction, useForwardProps } from 'reka-ui'
+
+const props = defineProps<AlertDialogActionProps>()
+
+const forwarded = useForwardProps(props)
+</script>
---
+++ packages/arvue-ui/src/alert-dialog/AlertDialogTrigger.vue
@@ -0,0 +1,17 @@
+<template>
+    <AlertDialogTrigger v-bind="props">
+        <slot/>
+    </AlertDialogTrigger>
+</template>
+
+<script lang="ts">
+import type { AlertDialogTriggerProps as RekaAlertDialogTriggerProps } from 'reka-ui'
+
+export interface AlertDialogTriggerProps extends RekaAlertDialogTriggerProps {}
+</script>
+
+<script setup lang="ts">
+import { AlertDialogTrigger } from 'reka-ui'
+
+const props = defineProps<AlertDialogTriggerProps>()
+</script>
