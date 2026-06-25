2ea28411133479adc2bcf30a08c355baa240437a
Xalindie <ilyaserdyukovmmm@gmail.com>
2026-06-25 19:45:01 +1100

    feat: add tooltip component

---
+++ packages/arvue-ui/src/tooltip/TooltipProvider.vue
@@ -0,0 +1,19 @@
+<template>
+    <TooltipProvider v-bind="props">
+        <slot/>
+    </TooltipProvider>
+</template>
+
+<script lang="ts">
+import type { TooltipProviderProps as RekaTooltipProviderProps } from 'reka-ui'
+
+export interface TooltipProviderProps extends RekaTooltipProviderProps {}
+</script>
+
+<script setup lang="ts">
+import { TooltipProvider } from 'reka-ui'
+
+const props = withDefaults(defineProps<TooltipProviderProps>(), {
+    delayDuration: 0,
+})
+</script>
---
+++ packages/arvue-ui/src/tooltip/index.ts
@@ -0,0 +1,18 @@
+export {
+    default as Tooltip,
+    type TooltipEmits,
+    type TooltipProps,
+} from './Tooltip.vue'
+export {
+    default as TooltipContent,
+    type TooltipContentEmits,
+    type TooltipContentProps,
+} from './TooltipContent.vue'
+export {
+    default as TooltipProvider,
+    type TooltipProviderProps,
+} from './TooltipProvider.vue'
+export {
+    default as TooltipTrigger,
+    type TooltipTriggerProps,
+} from './TooltipTrigger.vue'
