5aa2438780dcad39822be5293d09e751cfcd31d2
Xalindie <ilyaserdyukovmmm@gmail.com>
2026-06-28 19:32:31 +1100

    feat: add collapsible component

---
+++ packages/arvue-ui/src/collapsible/CollapsibleContent.vue
@@ -0,0 +1,18 @@
+<template>
+    <CollapsibleContent v-bind="props">
+        <slot/>
+    </CollapsibleContent>
+</template>
+
+<script lang="ts">
+import type { CollapsibleContentProps as RekaCollapsibleContentProps } from 'reka-ui'
+
+export interface CollapsibleContentProps extends RekaCollapsibleContentProps {
+}
+</script>
+
+<script setup lang="ts">
+import { CollapsibleContent } from 'reka-ui'
+
+const props = defineProps<CollapsibleContentProps>()
+</script>
---
+++ packages/arvue-ui/src/collapsible/story/Collapsible.story.vue
@@ -0,0 +1,49 @@
+<template>
+    <Story title="Collapsible/Demo">
+        <Variant title="default">
+            <Collapsible>
+                <CollapsibleTrigger>
+                    Trigger
+                </CollapsibleTrigger>
+                <span class="alert">
+                    .alert
+                </span>
+                <CollapsibleContent>
+                    <span class="alert success">
+                        <i class="fas fa-check-circle"/>
+                        .alert.success
+                    </span>
+                    <span class="alert danger">
+                        <span class="icon">
+                            <i class="fas fa-skull"/></span>
+                        .alert.danger
+                    </span>
+                    <span class="alert warning">
+                        <i class="fas fa-exclamation-triangle"/>
+                        .alert.warning
+                        <a
+                            href="#"
+                            class="alert-close"
+                        >
+                            <i class="fas fa-times"/>
+                        </a>
+                    </span>
+                    <span class="alert info">
+                        .alert.info
+                    </span>
+                    <span class="alert outlined">
+                        .alert.outlined
+                    </span>
+                </CollapsibleContent>
+            </Collapsible>
+        </Variant>
+    </Story>
+</template>
+
+<script setup lang="ts">
+import {
+    Collapsible,
+    CollapsibleContent,
+    CollapsibleTrigger,
+} from '@/collapsible'
+</script>
