387d834000e59778cebf05e0d3751a4f8d6496df
Ilya Serdyukov <ilyaserdyukovmmm@gmail.com>
2026-07-01 05:00:11 +1100

    feat: add slider component (#22)

---
+++ packages/arvue-ui/src/slider/Slider.vue
@@ -0,0 +1,46 @@
+<style>
+@import './slider.css';
+</style>
+
+<template>
+    <SliderRoot
+        v-slot="{ modelValue }"
+        :class="clsx('arvue-slider', props.class)"
+        v-bind="forwarded"
+    >
+        <SliderTrack class="arvue-slider-track">
+            <SliderRange class="arvue-slider-range"/>
+        </SliderTrack>
+
+        <SliderThumb
+            v-for="(_, key) in modelValue"
+            :key="key"
+            class="arvue-slider-thumb"
+        />
+    </SliderRoot>
+</template>
+
+<script lang="ts">
+import type { SliderRootEmits, SliderRootProps } from 'reka-ui'
+import type { HTMLAttributes } from 'vue'
+
+export interface SliderProps extends SliderRootProps {
+    class?: HTMLAttributes['class']
+}
+
+export interface SliderEmits extends SliderRootEmits {
+}
+</script>
+
+<script setup lang="ts">
+import { reactiveOmit } from '@vueuse/core'
+import { clsx } from 'clsx'
+import { SliderRange, SliderRoot, SliderThumb, SliderTrack, useForwardPropsEmits } from 'reka-ui'
+
+const props = defineProps<SliderProps>()
+const emits = defineEmits<SliderEmits>()
+
+const delegatedProps = reactiveOmit(props, 'class')
+
+const forwarded = useForwardPropsEmits(delegatedProps, emits)
+</script>
---
+++ packages/arvue-ui/src/slider/story/Slider.story.vue
@@ -0,0 +1,29 @@
+<template>
+    <Story title="Slider/Demo">
+        <Variant title="default">
+            <div style="height: 100vh; width: 100vw; display: flex; align-items: center; justify-content: center;">
+                <Slider style="width: 300px"/>
+            </div>
+        </Variant>
+        <Variant title="Vertical">
+            <div style="height: 100vh; width: 100vw; display: flex; align-items: center; justify-content: center;">
+                <Slider
+                    orientation="vertical"
+                    style="height: 300px"
+                />
+            </div>
+        </Variant>
+        <Variant title="Range">
+            <div style="height: 100vh; width: 100vw; display: flex; align-items: center; justify-content: center;">
+                <Slider
+                    :default-value="[25, 75]"
+                    style="width: 300px"
+                />
+            </div>
+        </Variant>
+    </Story>
+</template>
+
+<script setup lang="ts">
+import { Slider } from '@/slider'
+</script>
