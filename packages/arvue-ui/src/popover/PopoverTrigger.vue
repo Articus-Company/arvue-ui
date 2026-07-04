ee03f73dabb3dbb1dfd47b95c8ce07cf7397c779
Xalindie <ilyaserdyukovmmm@gmail.com>
2026-07-05 07:45:10 +1100

    feat: add popover component

---
+++ packages/arvue-ui/src/popover/PopoverTrigger.vue
@@ -0,0 +1,17 @@
+<template>
+    <PopoverTrigger v-bind="props">
+        <slot/>
+    </PopoverTrigger>
+</template>
+
+<script lang="ts">
+import type { PopoverTriggerProps as RekaPopoverTriggerProps } from 'reka-ui'
+
+export interface PopoverTriggerProps extends RekaPopoverTriggerProps {}
+</script>
+
+<script setup lang="ts">
+import { PopoverTrigger } from 'reka-ui'
+
+const props = defineProps<PopoverTriggerProps>()
+</script>
---
+++ packages/arvue-ui/src/popover/story/Popover.story.vue
@@ -0,0 +1,40 @@
+<template>
+    <Story title="Popover/Demo">
+        <Variant title="default">
+            <Popover>
+                <PopoverTrigger>
+                    Open popover
+                </PopoverTrigger>
+                <PopoverContent class="box">
+                    <div class="fields">
+                        <p class="heading">
+                            Popover
+                        </p>
+                        <p class="hint">
+                            description
+                        </p>
+                        <div class="field">
+                            <div class="name">
+                                name
+                            </div>
+                            <div class="value">
+                                <input
+                                    type="text"
+                                    class="full-width"
+                                >
+                            </div>
+                        </div>
+                    </div>
+                </PopoverContent>
+            </Popover>
+        </Variant>
+    </Story>
+</template>
+
+<script setup lang="ts">
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from '@/popover'
+</script>
