4aeccf8cb624f10e0fc80882719daed8ce679dfb
Xalindie <ilyaserdyukovmmm@gmail.com>
2026-07-05 08:34:05 +1100

    feat: add color swatch component

---
+++ packages/arvue-ui/src/color-swatch/ColorSwatch.vue
@@ -0,0 +1,32 @@
+<style>
+@import './color-swatch.css';
+</style>
+
+<template>
+    <ColorSwatch
+        v-slot="slotProps"
+        v-bind="delegatedProps"
+        :class="clsx('arvue-color-swatch', props.class)"
+    >
+        <slot v-bind="slotProps"/>
+    </ColorSwatch>
+</template>
+
+<script lang="ts">
+import type { ColorSwatchProps as RekaColorSwatchProps } from 'reka-ui'
+import type { HTMLAttributes } from 'vue'
+
+export interface ColorSwatchProps extends RekaColorSwatchProps {
+    class?: HTMLAttributes['class']
+}
+</script>
+
+<script setup lang="ts">
+import { reactiveOmit } from '@vueuse/core'
+import { clsx } from 'clsx'
+import { ColorSwatch } from 'reka-ui'
+
+const props = defineProps<ColorSwatchProps>()
+
+const delegatedProps = reactiveOmit(props, 'class')
+</script>
---
+++ packages/arvue-ui/src/color-swatch/story/ColorSwatch.story.vue
@@ -0,0 +1,11 @@
+<template>
+    <Story title="Color swatch/Demo">
+        <Variant title="default">
+            <ColorSwatch color="#0075cb"/>
+        </Variant>
+    </Story>
+</template>
+
+<script setup lang="ts">
+import { ColorSwatch } from '@/color-swatch'
+</script>
