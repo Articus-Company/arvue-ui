9783ba9e5af68c2c1a016cc1a328eb8455cde356
Xalindie <ilyaserdyukovmmm@gmail.com>
2026-07-05 10:47:08 +1100

    feat: add color area component

---
+++ packages/arvue-ui/src/color-area/ColorArea.vue
@@ -0,0 +1,42 @@
+<style>
+@import './color-area.css';
+</style>
+
+<template>
+    <ColorAreaRoot
+        v-slot="{ style }"
+        v-bind="forwarded"
+        class="arvue-color-area-root"
+    >
+        <ColorAreaArea
+            :class="clsx('arvue-color-area', props.class)"
+            :style="style"
+        >
+            <ColorAreaThumb class="arvue-color-area-thumb"/>
+        </ColorAreaArea>
+    </ColorAreaRoot>
+</template>
+
+<script lang="ts">
+import type { ColorAreaRootEmits, ColorAreaRootProps } from 'reka-ui'
+import type { HTMLAttributes } from 'vue'
+
+export interface ColorAreaProps extends ColorAreaRootProps {
+    class?: HTMLAttributes['class']
+}
+export interface ColorAreaEmits extends ColorAreaRootEmits {
+}
+</script>
+
+<script setup lang="ts">
+import { reactiveOmit } from '@vueuse/core'
+import { clsx } from 'clsx'
+import { ColorAreaArea, ColorAreaRoot, ColorAreaThumb, useForwardPropsEmits } from 'reka-ui'
+
+const props = defineProps<ColorAreaProps>()
+const emits = defineEmits<ColorAreaEmits>()
+
+const delegatedProps = reactiveOmit(props, 'class')
+
+const forwarded = useForwardPropsEmits(delegatedProps, emits)
+</script>
---
+++ packages/arvue-ui/src/color-area/story/ColorArea.story.vue
@@ -0,0 +1,11 @@
+<template>
+    <Story title="Color area/Demo">
+        <Variant title="default">
+            <ColorArea/>
+        </Variant>
+    </Story>
+</template>
+
+<script setup lang="ts">
+import { ColorArea } from '@/color-area'
+</script>
