cb21d8195ac21a4410049f1008dbb9b5230911c3
Xalindie <ilyaserdyukovmmm@gmail.com>
2026-07-17 14:01:56 +1100

    docs: add home page

---
+++ app/src/components/hero/featured-cards/FeatureCard1.vue
@@ -0,0 +1,29 @@
+<style scoped>
+.feat-img {
+    position: absolute;
+    width: 350px;
+    left: 50%;
+    bottom: -50px;
+    transform: translateX(-50%);
+}
+</style>
+
+<template>
+    <FeaturedCard>
+        <template #title>
+            Скорость работы пользователя
+        </template>
+        <template #description>
+            Компоненты реагируют мгновенно и поддерживают быстрый темп работы.
+        </template>
+        <img
+            class="feat-img"
+            src="/wa-apps/arvueui/img/lightning.png"
+            alt="lightning"
+        >
+    </FeaturedCard>
+</template>
+
+<script setup lang="ts">
+import { FeaturedCard } from '@/components/ui/featured-card'
+</script>
---
+++ app/src/components/hero/featured-cards/FeatureCard2.vue
@@ -0,0 +1,29 @@
+<style scoped>
+.feat-img {
+    position: absolute;
+    width: 100%;
+    left: 50%;
+    bottom: 0;
+    transform: translateX(-50%);
+}
+</style>
+
+<template>
+    <FeaturedCard>
+        <template #title>
+            Высокий DX
+        </template>
+        <template #description>
+            Полная типизация, автодополнение и предсказуемое API ускоряют разработку.
+        </template>
+        <img
+            class="feat-img"
+            src="/wa-apps/arvueui/img/dx.png"
+            alt="dx"
+        >
+    </FeaturedCard>
+</template>
+
+<script setup lang="ts">
+import { FeaturedCard } from '@/components/ui/featured-card'
+</script>
